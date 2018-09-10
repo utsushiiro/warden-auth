2decd6ddabeda35a76e99cc02ef369ed41a6651e
utsushiiro <ozeki.haru@gmail.com>
2018-09-10 20:22:06 +0900

    create simple rack app

---
+++ Application.rb
@@ -0,0 +1,5 @@
+class Application
+    def call(env)
+      ['200', {'Content-Type' => 'text/html'}, ['Hello Rack!']]
+    end
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+require 'rack'
+require './Application'
+
+run Application.new
