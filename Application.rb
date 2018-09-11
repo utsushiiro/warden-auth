785fc0c5beba4347bbb7eb47f637e42e61d71b22
utsushiiro <ozeki.haru@gmail.com>
2018-09-12 00:55:53 +0900

    change a message

--- Application.rb
+++ Application.rb
@@ -1,6 +1,6 @@
 class Application
     def call(env)
       env['warden'].authenticate!
-      ['200', {'Content-Type' => 'text/html'}, ['Hello Rack!']]
+      ['200', {'Content-Type' => 'text/html'}, ["Hello #{env['warden'].user.id}!"]]
     end
 end
