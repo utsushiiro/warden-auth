21db5c3b572c2d66c4ccd53da060b94a9dbb8d8c
utsushiiro <ozeki.haru@gmail.com>
2018-09-12 23:43:02 +0900

    authenticate! -> authenticate?

--- Application.rb
+++ Application.rb
@@ -8,11 +8,18 @@ class Application
         env['warden'].authenticate!
         ['200', {'Content-Type' => 'text/html'}, ["Hello #{env['warden'].user.id}!"]]
       when '/logout'
-        env['warden'].logout
-        ['200', {'Content-Type' => 'text/html'}, ["GoodBye!"]]
+        if env['warden'].authenticate?
+          env['warden'].logout
+          ['200', {'Content-Type' => 'text/html'}, ["GoodBye!"]]
+        else
+          ['401', {'Content-Type' => 'text/html'}, ['Unauthorized']]
+        end
       when '/protected'
-        env['warden'].authenticate!
-        ['200', {'Content-Type' => 'text/html'}, ['This is a protected page!']]
+        if env['warden'].authenticate?
+          ['200', {'Content-Type' => 'text/html'}, ['This is a protected page!']]
+        else
+          ['401', {'Content-Type' => 'text/html'}, ['Unauthorized']]
+        end
       end
     end
 end
