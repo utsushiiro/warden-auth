09b98624084193d8a57fd8122c49f72f7f3d5d90
utsushiiro <ozeki.haru@gmail.com>
2018-09-12 23:32:10 +0900

    add logout page

--- Application.rb
+++ Application.rb
@@ -1,6 +1,16 @@
 class Application
     def call(env)
-      env['warden'].authenticate!
-      ['200', {'Content-Type' => 'text/html'}, ["Hello #{env['warden'].user.id}!"]]
+      request = Rack::Request.new(env)
+      case request.path_info
+      when '/'
+        ['200', {'Content-Type' => 'text/html'}, ["Hello Rack!"]]
+      when '/login'
+        env['warden'].authenticate!
+        ['200', {'Content-Type' => 'text/html'}, ["Hello #{env['warden'].user.id}!"]]
+      when '/logout'
+        id = env['warden'].user.id
+        env['warden'].logout
+        ['200', {'Content-Type' => 'text/html'}, ["GoodBye #{id}!"]]
+      end
     end
 end
