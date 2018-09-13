f8fdeef6938bb365e89b3206ef9f8da68d10be58
utsushiiro <ozeki.haru@gmail.com>
2018-09-13 22:48:50 +0900

    add \n

--- Application.rb
+++ Application.rb
@@ -3,22 +3,22 @@ class Application
       request = Rack::Request.new(env)
       case request.path_info
       when '/'
-        ['200', {'Content-Type' => 'text/html'}, ["Hello Rack!"]]
+        ['200', {'Content-Type' => 'text/html'}, ["Hello Rack!\n"]]
       when '/login'
         env['warden'].authenticate!
-        ['200', {'Content-Type' => 'text/html'}, ["Hello #{env['warden'].user.id}!"]]
+        ['200', {'Content-Type' => 'text/html'}, ["Hello #{env['warden'].user.id}!\n"]]
       when '/logout'
         if env['warden'].authenticate?
           env['warden'].logout
-          ['200', {'Content-Type' => 'text/html'}, ["GoodBye!"]]
+          ['200', {'Content-Type' => 'text/html'}, ["GoodBye!\n"]]
         else
-          ['401', {'Content-Type' => 'text/html'}, ['Unauthorized']]
+          ['401', {'Content-Type' => 'text/html'}, ["Unauthorized\n"]]
         end
       when '/protected'
         if env['warden'].authenticate?
-          ['200', {'Content-Type' => 'text/html'}, ['This is a protected page!']]
+          ['200', {'Content-Type' => 'text/html'}, ["This is a protected page!\n"]]
         else
-          ['401', {'Content-Type' => 'text/html'}, ['Unauthorized']]
+          ['401', {'Content-Type' => 'text/html'}, ["Unauthorized\n"]]
         end
       end
     end
